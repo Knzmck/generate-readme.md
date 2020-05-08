4df5038e86155e93886c205b7d34574bed2af377
Mackenzie Schutz <mshoots64@gmail.com>
2020-05-08 16:00:40 -0500

    preview md

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -11,26 +11,35 @@ function generateMarkdown(data) {
    * Questions
    
    ## About 
+
    ${data.motivation}
+
    ${data.description}
 
    ## Installation 
+
    ${data.installation}
 
    ## Usage
+
    ${data.usage}
 
    ## License 
+
    ${data.license}
 
    ## Contributing 
+
    ${data.contributing}
+
    ${data.user}
 
    ## Tests 
+
    ${data.tests}
 
    ## Questions
+   
    ${data.questions}
 
    `;
